d9c37e8efd84b420d18dcb2c882e83858a0e64d4
Jeff Vincent <jeff.d.vincent@gmail.com>
2019-09-17 12:14:42 -0700

    Init

---
+++ rapidapipractice/api/views.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from .serializers import UserSerializer, GroupSerializer, AlbumSerializer
+from .models import Album
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+class AlbumViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows albums to be viewed or edited.
+    """
+    queryset = Album.objects.all()
+    serializer_class = AlbumSerializer
